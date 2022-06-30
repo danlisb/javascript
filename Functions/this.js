4ab89f8f34887a1f680bbaa65a1f6a3c5a2c54a7
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-06-30 03:14:10 -0300

    Update this.js

--- Functions/this.js
+++ Functions/this.js
@@ -1,7 +1,5 @@
 function calculaIdade(anos) {
-	return `Daqui a ${anos} anos, ${this.nome} terá ${
-		this.idade + anos
-	} anos de idade.`;
+	return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
 }
 
 const pessoa1 = {
